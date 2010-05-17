780c071646a6305ec4d1ceb35fc863dd746462f5
Dariusz Synowiec <darek129@gmail.com>
2010-05-17 14:55:13 +0200

    added startup script

---
+++ .bin/afterstart.rb
@@ -0,0 +1,33 @@
+require 'highline/import'
+require 'ezcrypto'
+
+p "After Start"
+
+tasks = [ 
+   { :name => 'kill_dropbox'   , :command => "taskkill /f /im Dropbox.exe"                                                        , :time => 20   } , 
+   { :name => 'kill_TSVNCache' , :command => "taskkill /f /im TSVNCache.exe"                                                      , :time => 5    } , 
+   { :name => 'start_Todolist' , :command => "C:\Darek\\app\\todolist\\ToDoList.exe"                                              , :time => 5    } , 
+   { :name => 'backup_jazdy'   , :command => "copy k:\\Shared\\synowiecd\\jazdy.xls c:\\Darek\\Doc\\dojazd /Y"                    , :time => 60   } , 
+   { :name => 'start_dropbox'  , :command => "C:\\Documents and Settings\\synowiecd\\Application Data\\Dropbox\\bin\\Dropbox.exe" , :time => 7*60 } , 
+]
+
+pass=EzCrypto::Key.generate
+
+key = pass.encrypt ask("Enter your password:  ") { |q| q.echo = "*" }
+
+current_tasks = %x[schtasks]
+
+tasks.each do |task|
+   if current_tasks =~ /#{task[:name]}/ then
+      system("schtasks /delete /tn #{task[:name]} /f")
+   end
+
+   system("schtasks /create /RU synowiecd /RP #{pass.decrypt(key)} /sc ONCE /tn #{task[:name]} /tr \"" + task[:command] + "\" /st " + (Time.now + (task[:time]*60)).strftime("%H:%M:%S") )
+end
+pass = nil
+keu = nil
+
+current_tasks = %x[schtasks]
+
+puts current_tasks
+
